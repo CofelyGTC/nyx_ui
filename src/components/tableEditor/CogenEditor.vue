2078522612ac25c901cd5fac743cdc9da0477f98
debockpierrecofely <pierre.debock@engie.com>
2019-10-03 17:42:38 +0200

    savepoint

--- src/components/tableEditor/CogenEditor.vue
+++ src/components/tableEditor/CogenEditor.vue
@@ -15,11 +15,14 @@
         <el-form-item label="Name" :label-width="formLabelWidth">
           <el-input :disabled="editMode!='create'" size="mini" v-model="newRec._source.name" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item label="Hours" :label-width="formLabelWidth">
-          <el-input-number size="mini" v-model="newRec._source.Hours" min="0"></el-input-number>
+        <el-form-item label="Last Update" :label-width="formLabelWidth">
+          <el-date-picker :disabled="editMode!='create'" v-model="newRec._source.LastUpdate" type="date" format="yyyy/MM/dd" value-format="timestamp" ></el-date-picker>
+        </el-form-item>
+        <el-form-item label="Hours" :label-width="formLabelWidth" >
+          <el-input-number size="mini" v-model="newRec._source.Hours" min=0></el-input-number>
         </el-form-item>
         <el-form-item label="Starts-Stops" :label-width="formLabelWidth">
-          <el-input-number size="mini" v-model="newRec._source.Starts" min="0"></el-input-number>
+          <el-input-number size="mini" v-model="newRec._source.Starts" min=0></el-input-number>
         </el-form-item>
       </el-card>
     </el-form>
@@ -106,11 +109,14 @@ export default {
         delete this.newRec._source.weather;
       }
 
+      this.newRec._source.modifyBy = this.$store.getters.creds.user.login
+      this.newRec._source.LastUpdate = Date.now()
+
       this.$store.commit({
         type: "updateRecord",
         data: this.newRec
       });
-      this.$notify({
+      this.$notify({ 
         title: "Record saved.",
         type: "success",
         message: "The cogen has been succesfuly saved.",
