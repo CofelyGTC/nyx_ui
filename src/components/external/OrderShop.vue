2e1100b860eeb591800170e58ab4218d03e8a5f0
debockpierrecofely <pierre.debock@engie.com>
2019-10-31 09:40:15 +0100

    save cogen editor

---
+++ src/components/external/OrderShop.vue
@@ -0,0 +1,489 @@
+<template>
+  <el-row class="ordershop-container" :span="24" type="flex" justify="center">
+      <el-form>
+          <el-row v-for="item in this.records" :key="_id" :span="24" class="row-record">
+
+
+          </el-row>    
+      </el-form>    
+  </el-row>
+</template>
+<script>
+import Vue from "vue";
+import moment,{ months } from "moment";
+import axios from "axios";
+
+export default {
+  name: "FormOrderShop",
+  data: () => ({
+      records: null,
+      editable: true,
+
+  }),
+  created: function() {
+    this.prepareData();
+  },
+  methods: {
+    prepareData() {
+      console.log('prepare data')
+      for(var i in this.$store.getters.creds.user.privileges) {
+        var priv = this.$store.getters.creds.user.privileges[i]
+        if(priv =='admin' ||  priv=='SHOP_FORM') {
+          this.writeAccess = true
+        }
+      }
+      this.monthSelected = moment()
+      this.dateSelected()
+    },
+    dateSelected() {
+      
+      if(this.monthSelected == null)
+        this.monthSelected = moment()
+
+      if(moment().format('D') >= 14) {
+        this.disable = (moment() > moment(this.monthSelected).endOf('Month'))
+      }
+      else {
+        this.disable = (moment().subtract(1, 'months') > moment(this.monthSelected).endOf('Month'))
+      }
+        
+      this.strPeriod = moment(this.monthSelected).startOf('Month').format('DD MMM YYYY')+' to '+moment(this.monthSelected).endOf('Month').format('DD MMM YYYY')
+      this.getData()
+    },
+    getData() {
+      var url =
+      this.$store.getters.apiurl +
+      "generic_search/products_parameters?token=" +
+      this.$store.getters.creds.token;
+
+      var query = {
+        size: 2000,
+        query: {
+          bool: {
+            must: [
+              {
+                match_all: {}
+              }
+            ]
+          }
+        }
+      };
+
+      axios
+        .post(url, query)
+        .then((response) => {
+          if(response.data.error!="")
+            console.log("Order Shops Calls list error...");
+          else
+          {
+            this.callData=[]
+            for(var i in response.data.records) {
+              response.data.records[i]._source._id = response.data.records[i]._id 
+              console.log("Retrieved data : " + JSON.parse(JSON.stringify(response.data)))
+
+              this.callData.push(response.data.records[i]._source)
+            }
+
+            
+                var tmp = JSON.parse(JSON.stringify(this.callData))
+                this.records = null
+                this.records = JSON.parse(JSON.stringify(tmp))
+          }});
+
+          },
+
+    handleChangeNumberCall : _.debounce(function() {
+          var obj = { 
+            'month_to_update': moment(this.monthSelected).format('YYYY-MM-DD'),
+          }
+          
+          for(var i in this.lotModel) {
+            obj['number_of_call_'+this.lotModel[i].value] = this.lotModel[i].numberOfCall
+          }
+          
+          console.log(obj)
+
+          axios.post(
+            this.$store.getters.apiurl + "biac/kpi101_monthly?token="+this.$store.getters.creds.token, obj)
+            .then((response) => {
+              if(response.data.error!="")
+                console.log("KPI101 update monthly error");
+              else {
+                console.log("KPI101 update monthly success");
+                console.log(response.data.data)
+
+
+                var res = JSON.parse(response.data.data)
+
+                try {
+                  for(var i in this.lotModel) {
+                    var lotModel = this.lotModel[i]
+                    lotModel.numberOfCall = res['number_of_call_'+lotModel.value]
+                    lotModel.notTimelyAnswered = res['not_timely_answer_'+lotModel.value]
+                    
+                    if(lotModel.numberOfCall == 0)
+                      lotModel.percentage = 0
+                    else {
+                      var percent = 100*((lotModel.numberOfCall-lotModel.notTimelyAnswered)/lotModel.numberOfCall)
+                      lotModel.percentage = Math.round(percent*100)/100
+                    }
+                  }
+                }
+                catch(error) {
+                  console.log(error)
+                  for(var i in this.lotModel) {
+                    this.lotModel[i].percentage = 0
+                  }
+                }
+
+                this.$notify({
+                  title: "Success",
+                  message: "Data Saved",
+                  type: "success",
+                  position: "bottom-right"
+                });
+              }
+          })
+          .catch((error)=> {
+            console.log(error);
+          });
+        
+      }, 500),
+    setTableCurrent(row) {
+      this.$refs.callTable.setCurrentRow(row);
+    },
+    handleTableSelect(val) {
+      if(this.disable)
+        return
+
+      if(val == null)
+        return
+
+      this.dialogVisible = true
+      this.dialogType = 'modif'
+      this.currentRow = val;
+      this.dialogTitle   = 'Ticket '+val.number
+
+      this.dialogObj = JSON.parse(JSON.stringify(this.currentRow))
+
+      for(var j in this.lotModel) {
+        if(this.lotModel[j].value == this.dialogObj.lot)
+          this.dialogObj.lot = this.lotModel[j].label
+      }
+
+      this.setTableCurrent()
+    },
+    addCall() {
+      this.dialogObj = {
+        datetime: moment(),
+        number: '',
+      }
+      this.dialogType = 'creation'
+      this.dialogTitle   = 'Nieuwe oproep'
+      this.dialogVisible = true
+
+    },
+    clickDialogDelete() {
+
+      this.$confirm(
+        "This will permanently delete the call. Continue?",
+        "Warning",
+        {
+          confirmButtonText: "OK",
+          cancelButtonText: "Cancel",
+          type: "warning"
+        })
+        .then(() => {
+          this.deleteLoading = true
+          var delRec = {
+            _id: this.currentRow._id,
+            _index: 'biac_kpi101_call',
+            _doc: 'doc'
+          }
+    
+          this.$store.commit({
+            type: "deleteRecord",
+            data: delRec
+          });
+    
+    
+          setTimeout(() => { 
+            this.getData()
+            this.dialogVisible = false   
+            this.deleteLoading = false
+            
+            this.$notify({
+              title: "Success",
+              message: "call "+this.currentRow.number+" has been deleted",
+              type: "success",
+              position: "bottom-right"
+            });
+    
+          }, 500);
+    
+          setTimeout(() => this.getData(), 1500)
+
+          setTimeout(() => {
+            console.log('**************************************')
+            console.log(moment(this.monthSelected).format('YYYY-MM-DD'))
+            
+            axios.post(
+              this.$store.getters.apiurl + "biac/kpi101_monthly?token="+this.$store.getters.creds.token,
+                { 'month_to_update': moment(this.monthSelected).format('YYYY-MM-DD') }
+              ).then((response) => {
+                if(response.data.error!="")
+                  console.log("KPI104 update monthly error");
+                else {
+                  console.log("KPI104 update monthly success");
+                  console.log(response)
+                }
+            })
+            .catch((error)=> {
+              console.log(error);
+            });
+          }, 2500)
+        })
+    },
+    clickDialogModify() {
+      this.loading = true
+      this.$refs['formCall'].validate((valid) => {
+        if (valid) {
+          var id = this.dialogObj._id
+          delete this.dialogObj._id
+          delete this.dialogObj.hour
+          delete this.dialogObj.date
+
+          try {
+            delete this.dialogObj.madeBy
+            delete this.dialogObj.phoneNumber
+          }
+          catch {
+
+          }
+          
+          for(var j in this.lotModel) {
+            if(this.lotModel[j].label == this.dialogObj.lot)
+              this.dialogObj.lot = this.lotModel[j].value
+          }
+
+          var newRec = {
+            _id: id,
+            _index: "biac_kpi101_call",
+            isadd:false,
+            _type: "doc",
+
+            _source: this.dialogObj
+          }
+
+          this.$store.commit({
+            type: "updateRecord",
+            data: newRec
+          });
+
+          this.currentMonth = moment(this.monthSelected)
+          this.targetMonth  = moment(newRec._source.datetime)
+
+          console.log('currentMonth: ' + this.currentMonth.format('YYYY-MM-DD'))
+          console.log(' targetMonth: ' + this.targetMonth.format('YYYY-MM-DD'))
+
+          setTimeout(() => { 
+            this.getData()
+            this.dialogVisible = false
+            this.loading = false
+            this.$notify({
+              title: "Success",
+              message: "call "+newRec._source.number+" has been modified",
+              type: "success",
+              position: "bottom-right"
+            });
+            this.currentRow = null
+
+          }, 500);
+
+          setTimeout( () => this.getData(), 1500)
+
+          
+          setTimeout(() => {
+            console.log('******************target month********************')
+            console.log(this.targetMonth.format('YYYY-MM-DD'))
+            
+            axios.post(
+              this.$store.getters.apiurl + "biac/kpi101_monthly?token="+this.$store.getters.creds.token,
+                { 'month_to_update': this.targetMonth.format('YYYY-MM-DD') }
+              ).then((response) => {
+                if(response.data.error!="")
+                  console.log("KPI104 update monthly error");
+                else {
+                  console.log("KPI104 update monthly success");
+                  console.log(response)
+                }
+            })
+            .catch((error)=> {
+              console.log(error);
+              
+            });
+          }, 2500)
+
+
+          if(this.targetMonth != this.currentMonth) {
+            setTimeout(() => {
+              console.log('***************current month***********************')
+              console.log(this.currentMonth.format('YYYY-MM-DD'))
+              
+              axios.post(
+                this.$store.getters.apiurl + "biac/kpi101_monthly?token="+this.$store.getters.creds.token,
+                  { 'month_to_update': this.currentMonth.format('YYYY-MM-DD') }
+                ).then((response) => {
+                  if(response.data.error!="")
+                    console.log("KPI104 update monthly error");
+                  else {
+                    console.log("KPI104 update monthly success");
+                    console.log(response)
+                  }
+              })
+              .catch((error)=> {
+                console.log(error);
+                
+              });
+            }, 2500)
+          }
+        } 
+        else {
+          this.loading = false
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    clickDialogCreate() {
+      this.loading = true
+      this.$refs['formCall'].validate((valid) => {
+        if (valid) {
+          
+          for(var j in this.lotModel) {
+            if(this.lotModel[j].label == this.dialogObj.lot)
+              this.dialogObj.lot = this.lotModel[j].value
+          }
+
+          var newRec = {
+            _id: "id_" + Math.floor((1 + Math.random()) * 0x1000000),
+            _index: "biac_kpi101_call",
+            isadd:true,
+            _type: "doc",
+
+            _source: this.dialogObj
+          }
+
+          console.log(newRec)
+
+          this.$store.commit({
+            type: "updateRecord",
+            data: newRec
+          });
+
+
+          this.currentMonth = moment(this.monthSelected)
+          this.targetMonth  = moment(newRec._source.datetime)
+
+          console.log('currentMonth: ' + this.currentMonth.format('YYYY-MM-DD'))
+          console.log(' targetMonth: ' + this.targetMonth.format('YYYY-MM-DD'))
+
+          setTimeout(function() { 
+            this.getData()
+            this.dialogVisible = false
+            this.loading = false
+            this.$notify({
+              title: "Success",
+              message: "call "+newRec._source.number+" has been CREATED",
+              type: "success",
+              position: "bottom-right"
+            });
+            this.currentRow = null
+
+          }.bind(this), 500);
+
+          setTimeout( () => this.getData(), 1500)
+
+
+          this.targetMonth = moment(newRec._source.datetime)
+          
+          
+          setTimeout(() => {
+            console.log('******************target month********************')
+            console.log(this.targetMonth.format('YYYY-MM-DD'))
+            
+            axios.post(
+              this.$store.getters.apiurl + "biac/kpi101_monthly?token="+this.$store.getters.creds.token,
+                { 'month_to_update': this.targetMonth.format('YYYY-MM-DD') }
+              ).then((response) => {
+                if(response.data.error!="")
+                  console.log("KPI104 update monthly error");
+                else {
+                  console.log("KPI104 update monthly success");
+                  console.log(response)
+                }
+            })
+            .catch((error)=> {
+              console.log(error);
+            });
+          }, 2500)
+
+          if(this.targetMonth != this.currentMonth) {
+            setTimeout(() => {
+              console.log('***************current month***********************')
+              console.log(this.currentMonth.format('YYYY-MM-DD'))
+              
+              axios.post(
+                this.$store.getters.apiurl + "biac/kpi101_monthly?token="+this.$store.getters.creds.token,
+                  { 'month_to_update': this.currentMonth.format('YYYY-MM-DD') }
+                ).then((response) => {
+                  if(response.data.error!="")
+                    console.log("KPI104 update monthly error");
+                  else {
+                    console.log("KPI104 update monthly success");
+                    console.log(response)
+                  }
+              })
+              .catch((error)=> {
+                console.log(error);
+              });
+            }, 2500)
+          }
+        } 
+        else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    }
+  }
+};
+
+</script>
+<style>
+.kpi101-container{
+  width: 100%;
+  margin: 30px 0px;
+  
+}
+.box-card {
+  width: 800px !important;
+}
+
+.first-row{
+  margin: 10px;
+}
+.second-row{
+  margin: 20px 10px;
+}
+.third-row{
+  margin: 20px 10px;
+}
+.forth-row{
+  margin: 20px 10px;
+}
+
+.table-disable {
+  cursor: not-allowed;
+}
+</style>
--- src/components/tableEditor/CogenEditor.vue
+++ src/components/tableEditor/CogenEditor.vue
@@ -18,15 +18,17 @@
         <el-form-item label="Last Update" :label-width="formLabelWidth">
           <el-date-picker :disabled="editMode!='create'" v-model="newRec._source.LastUpdate" type="date" format="yyyy/MM/dd" value-format="timestamp" ></el-date-picker>
         </el-form-item>
-        <el-form-item label="Maintenance" :label-width="formLabelWidth">
-           <el-checkbox v-model="checked">Maintenance done ?</el-checkbox>
-        </el-form-item>
         <el-form-item label="Hours" :label-width="formLabelWidth" >
           <el-input-number size="mini" v-model="newRec._source.Hours" min=0></el-input-number>
         </el-form-item>
         <el-form-item label="Starts-Stops" :label-width="formLabelWidth">
           <el-input-number size="mini" v-model="newRec._source.Starts" min=0></el-input-number>
         </el-form-item>
+        <el-form-item label="Entretiens réalisés" :label-width="formLabelWidth">
+          <el-checkbox-group v-model="maintenancesMade">
+            <el-checkbox v-for="maintenance in maintenances" v-model="maintenance.maintenanceDone" :label="maintenance._id" :key="maintenance.maintenanceDone">{{maintenance._id}} ({{maintenance._source.maintenance_interval}} heures)</el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>  
       </el-card>
     </el-form>
 
@@ -53,7 +55,8 @@ export default {
   data: () => ({
     orgRec: null,
     newRec: null,
-    checked: false,
+    maintenances: null,
+    maintenancesMade: [],
     strOrgRec: "",
     strNewRec: "",
     orgName: "",
@@ -71,7 +74,7 @@ export default {
       return JSON.stringify(this.record);
     },
     recchanged: function() {
-      return JSON.stringify(this.recordin) != JSON.stringify(this.newRec);
+      return ((JSON.stringify(this.recordin) != JSON.stringify(this.newRec)) || (this.maintenancesMade.length > 0)) ;
     }
   },
   props: {
@@ -107,7 +110,48 @@ export default {
       this.dialogFormVisible = true;
       this.newRec = JSON.parse(JSON.stringify(this.record));
       this.orgRec = JSON.parse(JSON.stringify(this.record));
+      this.maintenances = this.getMaintenances();
     },
+    getMaintenances: function() {
+        var url =
+        this.$store.getters.apiurl + "generic_search/maintenance_parameters?token=" +this.$store.getters.creds.token;
+        var query = {
+          size: 1000,
+          query: {
+              bool: {
+                must: [
+                  {
+                    match_phrase: {
+                      Motor: this.newRec._source.Motor
+                    }
+                  }
+                ]
+              }
+            }
+        };
+      axios
+        .post(url, query)
+        .then(response => {
+
+          //console.log(JSON.stringify(response))
+
+          this.loadMaintenance(response.data.records);
+
+        })
+        
+      }, 
+    
+    loadMaintenance: async function(recordsdata) {
+
+      console.log(JSON.stringify(recordsdata))
+
+     this.maintenances = recordsdata
+     this.maintenances.forEach(function(entry){
+       entry.maintnenanceDone = false
+     });
+
+    },
+    
     saveRecord: function() {
       if (!this.weatherActivated && this.newRec._source.weather != null) {
         delete this.newRec._source.weather;
@@ -115,9 +159,13 @@ export default {
 
       this.newRec._source.modifyBy = this.$store.getters.creds.user.login
       this.newRec._source.LastUpdate = Date.now()
-      if (this.checked){
+      
+      if (this.maintenancesMade.length > 0){
         this.newRec._source.last_maintenance = this.newRec._source.LastUpdate
+        this.newRec._source.maintenancesMade = this.maintenancesMade
       }
+
+
 
       this.$store.commit({
         type: "updateRecord",
@@ -131,7 +179,7 @@ export default {
       });
       this.$emit("dialogcloseupdated");
     }
-  }
+    }
 };
 </script>
 
