177618998f1a9512504fc24e6ccdc75dc32eadfd
lapin <va.mercier@gmail.com>
2019-11-06 11:33:54 +0100

    diffusion list editor

--- src/store/store.js
+++ src/store/store.js
@@ -32,7 +32,7 @@ export default new Vuex.Store({
   state: {
     apiurl: "api/v1/",
     kibanaurl:"/kibana/",
-    version: "v2.10.0",
+    version: "v2.10.2",
     devMode: false,
     menus: [],
     menuOpen: true,
