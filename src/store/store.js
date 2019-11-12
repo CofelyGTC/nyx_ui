1160b2f22e790a561834e014a86beed2dc015a6e
debockpierrecofely <pierre.debock@engie.com>
2019-11-12 16:56:28 +0100

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -32,7 +32,7 @@ export default new Vuex.Store({
   state: {
     apiurl: "api/v1/",
     kibanaurl:"/kibana/",
-    version: "v2.10.4",
+    version: "v2.10.5",
     devMode: false,
     menus: [],
     menuOpen: true,
