956e07c5adedf85d2730705d12b190b2bb33017b
lapin <va.mercier@gmail.com>
2019-11-28 14:51:38 +0100

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -32,7 +32,7 @@ export default new Vuex.Store({
   state: {
     apiurl: "api/v1/",
     kibanaurl:"/kibana/",
-    version: "v2.15.2",
+    version: "v2.15.3",
     devMode: false,
     menus: [],
     menuOpen: true,
