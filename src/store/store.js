3d53a596e659bdb93fe6bb0395e046bf33f97a58
debockpierrecofely <pierre.debock@engie.com>
2020-02-19 08:46:09 +0100

    Save point

--- src/store/store.js
+++ src/store/store.js
@@ -34,7 +34,7 @@ export default new Vuex.Store({
   state: {
     apiurl: "api/v1/",
     kibanaurl:"/kibana/",
-    version: "v2.27.2",
+    version: "v2.27.4",
     devMode: false,
     menus: [],
     menuOpen: true,
