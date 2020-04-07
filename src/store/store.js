f0eb2df63950b102136d567c8f3beeb11836f33d
lapin <va.mercier@gmail.com>
2020-04-07 15:11:35 +0200

    v3.16.1

--- src/store/store.js
+++ src/store/store.js
@@ -36,7 +36,7 @@ export default new Vuex.Store({
     apiurl: "api/v1/",
     apiVersion: "",
     kibanaurl: "/kibana/",
-    version: "v3.16.0",
+    version: "v3.16.1",
     devMode: false,
     menus: [],
     menuOpen: true,
