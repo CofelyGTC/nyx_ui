6aeadca1a24940624786de1acf9cd1d0b96a2c30
Arnaud Marchand <amarchand@icloud.com>
2020-04-09 14:23:03 +0200

    Save point

--- src/store/store.js
+++ src/store/store.js
@@ -38,7 +38,7 @@ export default new Vuex.Store({
     apiurl: "api/v1/",
     apiVersion: "",
     kibanaurl: "/kibana/",
-    version: "v3.17.1",
+    version: "v3.17.2",
     devMode: false,
     menus: [],
     menuOpen: true,
@@ -125,7 +125,7 @@ export default new Vuex.Store({
           //var socket = new WebSocket('wss://test2.nyx-ds.com/nyx_ui_websocket/');
           //var wsurl=context.getters.apiurl.replace("http://","ws://").replace("https://","wss://").replace("/api/v1","/nyx_ui_websocket/");
           var mainurl=extractURLParts(window.location.href);
-          var wsurl=mainurl.protocol+"://"+mainurl.host+"/nyx_ui_websocket/";          
+          var wsurl=mainurl.protocol+"//"+mainurl.host+"/nyx_ui_websocket/";          
           var socket = new WebSocket(wsurl);
           // Connection opened
           socket.addEventListener('open', function (event) {
