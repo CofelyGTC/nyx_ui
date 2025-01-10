3be09429938af856d8625aa127644b749092c1a2
erkanbulduk <erkan.bulduk@equans.com>
2025-01-10 11:08:35 +0100

    add auto-docker and build & push 3.27.7

---
+++ auto-docker.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+VERSION="3.27.7"
+sed -i '' -e "s/version:.*\",/version: \"v$VERSION\",/" ./src/store/store.js #For MACOS
+sed -i "s/version:.*\",/version: \"v$VERSION\",/" ./src/store/store.js  #For Windows
+docker build -t engiecofely/nyx_ui:v$VERSION .
+docker push engiecofely/nyx_ui:v$VERSION
+echo -en "\007"
