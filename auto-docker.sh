e5c3ee5ccec1da7165b44afb97c91da3b99fc23a
erkanbulduk <erkan.bulduk@equans.com>
2025-01-13 15:13:25 +0100

    change version

--- auto-docker.sh
+++ auto-docker.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-VERSION="3.27.7"
+VERSION="3.27.8"
 sed -i '' -e "s/version:.*\",/version: \"v$VERSION\",/" ./src/store/store.js #For MACOS
 sed -i "s/version:.*\",/version: \"v$VERSION\",/" ./src/store/store.js  #For Windows
 docker build -t engiecofely/nyx_ui:v$VERSION .
