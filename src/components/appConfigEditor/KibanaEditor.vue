2abb6ada9ca76b3535d1e1f85dca474b45e71f22
Julian Fickers <96172826+julianfickerseq@users.noreply.github.com>
2025-04-23 15:37:36 +0200

    working commit

--- src/components/appConfigEditor/KibanaEditor.vue
+++ src/components/appConfigEditor/KibanaEditor.vue
@@ -6,7 +6,7 @@
         <el-row>
           <el-col :span="8">
             <el-form-item label="Hidden Query" :label-width="formLabelWidth">
-              <el-input size="mini" v-model="currentConfig.config.hiddenQuery" autocomplete="off"></el-input>
+              <el-input size="mini" @blur="computeUrl" v-model="currentConfig.config.hiddenQuery" autocomplete="off"></el-input>
             </el-form-item>
           </el-col>
 
@@ -120,12 +120,14 @@
           <el-col :span="8">
             <el-form-item label :label-width="formLabelWidth">
             <el-row>
-              <el-switch v-model="currentConfig.hideFilter" active-text="Hide Filter"></el-switch>
+              <el-switch v-model="currentConfig.hideFilter"
+              @change="computeUrl"
+              active-text="Hide Filter"></el-switch>
             </el-row>
             <el-row>
               <el-switch
                 v-model="currentConfig.timeRefresh"
-                @change="timeRefreshSwitchChange"
+                @change="computeUrl"
                 active-text="Time Refresh"
               ></el-switch>
             </el-row>
@@ -196,7 +198,7 @@ export default {
     );
   },
   computed: {
-    curConfigIn: function() {
+    currentConfigIn: function() {
       return this.currentConfig;
     },
     fieldList: function() {
@@ -209,48 +211,35 @@ export default {
   },
   created: function() {
     this.prepareData();
+    console.log("created - this.currentConfig",this.currentConfig);
   },
   methods: {
+    computeUrl(){
+      this.computeUrlFromKibana()
+    },
     query_filter_changed:function() {
-        if(this.curConfig.queryFilterChecked)
-          this.curConfig.queryBarChecked=false;
-
+        if(this.currentConfig.queryFilterChecked){
+          this.currentConfig.queryBarChecked=false;
+        }
+        this.computeUrlFromKibana()
     },
     query_bar_changed:function() {
-        if(this.curConfig.queryBarChecked)
-          this.curConfig.queryFilterChecked=false;
-
+        if(this.currentConfig.queryBarChecked){
+          this.currentConfig.queryFilterChecked=false;
+        }
+        this.computeUrlFromKibana()
     },
     timeSelectorTypeChange() {
-      if (this.curConfig.timeSelectorType != null)
-        this.curConfig.timeSelectorChecked = true;
-
-      var tmp = JSON.parse(JSON.stringify(this.curConfig));
-      this.curConfig = null;
-      this.curConfig = tmp;
+      if (this.currentConfig.timeSelectorType != null){
+        this.currentConfig.timeSelectorChecked = true;
+      }
+      this.computeUrlFromKibana()
     },
     timeRefreshSelectChange() {
-      if (this.curConfig.timeRefreshValue != null)
-        this.curConfig.timeRefresh = true;
-
-
+      if (this.currentConfig.timeRefreshValue != null){
+        this.currentConfig.timeRefresh = true;
+      }
       this.computeUrlFromKibana();
-
-
-      var tmp = JSON.parse(JSON.stringify(this.curConfig));
-      this.curConfig = null;
-      this.curConfig = tmp;
-    },
-    
-    timeRefreshSwitchChange() {
-      this.computeUrlFromKibana();
-
-
-      var tmp = JSON.parse(JSON.stringify(this.curConfig));
-      this.curConfig = null;
-      this.curConfig = tmp;
-
-      console.log(this.curConfig.config.url)
     },
     prepareData() {
       this.loadKibanaDashboards();
@@ -269,287 +258,75 @@ export default {
       console.log("kibana time changed");
       this.computeUrlFromKibana();
     },
-    computeUrlFromKibana() {
-      console.log("computeUrlFromKibana");
-      if (this.selectedDash != null) {
-        var space = "";
-        if (this.selectedDash.space != "default") {
-          space = "s/" + this.selectedDash.space; //.toLowerCase();
-        }
-        this.currentConfig.config.url =
-          "." +
-          this.$store.getters.kibanaurl.replace("kibana", "kibananyx") +
-          space +
-          "/app/kibana#" +
-          this.computeKibanaUrlFromSelectedDash();
-      }
-    },
+
     kibanaDashboardSelected() {
       for (var i in this.dashboards) {
         if (this.dashboards[i].id == this.currentConfig.config.kibanaId) {
-          console.log("FOUND");
           this.selectedDash = this.dashboards[i];
-          console.log(this.dashboards[i]);
+          this.currentConfig.config.space = this.selectedDash.space
         }
       }
-      var space = "";
-      if (this.selectedDash.space != "default") {
-        space = "s/" + this.selectedDash.space; //.toLowerCase();
-      }
+      this.computeUrlFromKibana()
+    },
 
-      this.currentConfig.config.url =
+    computeUrlFromKibana() {
+      console.log("computeUrlFromKibana");
+      if (this.currentConfig.config.kibanaId != null) {
+        var space = this.currentConfig.config.space != "default" ? `s/${this.currentConfig.config.space}/`: ""
+        this.currentConfig.config.url =
         "." +
         this.$store.getters.kibanaurl.replace("kibana", "kibananyx") +
-        space +
-        "/app/kibana#" +
+        `app/${space}` +
         this.computeKibanaUrlFromSelectedDash();
-
-      var tmp = JSON.parse(JSON.stringify(this.currentConfig));
-      this.currentConfig = null;
-      this.currentConfig = tmp;
-      console.log(this.currentConfig)
-
+      }
     },
+
     computeKibanaUrlFromSelectedDash() {
-      console.log('computeKibanaUrlFromSelectedDash')
-      var dashdata = this.selectedDash;
-      var url = "";
-      url += "/dashboard/" + dashdata.id + "";
+      var url = "dashboards#/view/" + this.currentConfig.config.kibanaId + "?embed=true";
+      console.log(this.currentConfig);
+      if(this.currentConfig.hideFilter) url += "&hide-filter-bar=" + this.currentConfig.hideFilter;
+      if(this.currentConfig.queryBarChecked) url += "&show-query-input=" + this.currentConfig.queryBarChecked;
+      
       var timek = "from:now-7d,mode:quick,to:now";
       if (
         this.currentConfig.config != undefined &&
         this.currentConfig.config.kibanaTime != undefined
       ) {
-        console.log("saving kibana time");
-        console.log(this.currentConfig.config.kibanaTime);
         timek = this.currentConfig.config.kibanaTime;
       }
-
-      console.log(timek);
       this.currentConfig.config.kibanaTime = timek;
-
-      var strRefresh = "refreshInterval:(pause:!t,value:0)";
-
-      if (this.currentConfig.timeRefresh && this.currentConfig.timeRefreshValue != null)
-        strRefresh = 'refreshInterval:(pause:!f,value:'+this.currentConfig.timeRefreshValue+')'
-
-      url +=
-        "?fullScreenMode&hide-filter-bar=true&embed=true&_g=(" + strRefresh + ",time:(" + timek + "))";
-      url += "&_a=(description:'" + dashdata.attributes.description + "'";
-      url += ",filters:!(),fullScreenMode:!f";
-      if (dashdata.attributes.optionsJSON != undefined) {
-        var options = JSON.parse(dashdata.attributes.optionsJSON);
-        var urloptions = Object.entries(options)
-          .map(e => e.join(":"))
-          .join(",");
-        urloptions = urloptions.replace(/true/g, "!t").replace(/false/g, "!f");
-
-        url += ",options:(" + urloptions + ")";
-      } else url += ",options:()";
-
-      var panels = [];
-      var panelsJSON = JSON.parse(dashdata.attributes.panelsJSON);
-
-      for (var p in panelsJSON) {
-        console.log("==========+>");
-        console.log(panelsJSON[p]);
-
-        if (
-          dashdata.migrationVersion != null &&
-          dashdata.migrationVersion.dashboard != null &&
-          dashdata.migrationVersion.dashboard == "7.0.0"
-        ) {
-          for (var j in dashdata.references) {
-            if (dashdata.references[j].name == panelsJSON[p].panelRefName) {
-              panelsJSON[p].id = dashdata.references[p].id;
-              panelsJSON[p].type = dashdata.references[p].type;
-            }
-          }
-        }
-
-        var panel = transformObject(panelsJSON[p]);
-
-        panels.push("" + panel + "");
-      }
-
-      panels = panels
-        .join(",")
-        .replace(/#/g, "%23")
-        .replace(/&/g, "%26");
-      // url += ",panels:!(" + panels + ")";
-
-      var querybag = "query:(language:lucene,query:'*')";
-      if (
-        dashdata.attributes.kibanaSavedObjectMeta != undefined &&
-        dashdata.attributes.kibanaSavedObjectMeta.searchSourceJSON != undefined
-      ) {
-        var querybag2 = JSON.parse(
-          dashdata.attributes.kibanaSavedObjectMeta.searchSourceJSON
-        );
-        if (querybag2.query != undefined) {
-          querybag = transformObject(querybag2.query);
-          querybag = "query:" + querybag;
-        }
-      }
-
-      // url += "," + querybag + ",timeRestore:!f,title:Test,viewMode:view)";
-
-      url += ")";
       
-      console.log(
-        "********************compute kibana url***********************"
-      );
-      console.log('url: ', url);
-
-      return url;
-    },
+      
+      var strRefresh = "refreshInterval:(pause:!t,value:0)";
+      if (this.currentConfig.timeRefresh && this.currentConfig.timeRefreshValue != null)
+      strRefresh = 'refreshInterval:(pause:!f,value:'+this.currentConfig.timeRefreshValue+')'
     
-    loadKibanaDashboards: function() {
-      this.listLoading = true;
-      this.dashboards = [];
-
-      this.addDashboards(
-        {
-          page: 1,
-          per_page: 1000,
-          total: 4,
-          saved_objects: [
-            {
-              type: "dashboard",
-              id: "ede7b7b0-eb5e-11e9-b31c-91fd0a97e7fc",
-              attributes: {
-                title: "LUTOSA - Calendar",
-                hits: 0,
-                description: "",
-                panelsJSON: [
-                  {
-                    embeddableConfig: {},
-                    gridData: { h: 20, i: "1", w: 41, x: 0, y: 0 },
-                    id: "67239870-eb5e-11e9-b31c-91fd0a97e7fc",
-                    panelIndex: "1",
-                    title: "Calendar",
-                    type: "visualization",
-                    version: "6.5.4"
-                  },
-                  {
-                    embeddableConfig: {},
-                    gridData: { h: 10, i: "2", w: 7, x: 41, y: 0 },
-                    id: "83795190-eb5e-11e9-b31c-91fd0a97e7fc",
-                    panelIndex: "2",
-                    title: "Days",
-                    type: "visualization",
-                    version: "6.5.4"
-                  },
-                  {
-                    embeddableConfig: {},
-                    gridData: { h: 10, i: "3", w: 7, x: 41, y: 10 },
-                    id: "9faf26a0-eb5e-11e9-b31c-91fd0a97e7fc",
-                    panelIndex: "3",
-                    title: "Open days",
-                    type: "visualization",
-                    version: "6.5.4"
-                  },
-                  {
-                    embeddableConfig: {},
-                    gridData: { h: 10, i: "4", w: 48, x: 0, y: 20 },
-                    id: "342e2c20-eb6b-11e9-b31c-91fd0a97e7fc",
-                    panelIndex: "4",
-                    title: "Targets",
-                    type: "visualization",
-                    version: "6.5.4"
-                  }
-                ],
-                optionsJSON: {
-                  darkTheme: false,
-                  hidePanelTitles: false,
-                  useMargins: true
-                },
-                version: 1,
-                timeRestore: false,
-                kibanaSavedObjectMeta: {
-                  searchSourceJSON: {
-                    query: { language: "lucene", query: "" },
-                    filter: []
-                  }
-                }
-              },
-              updated_at: "2019-10-10T14:45:45.529Z",
-              version: 5
-            },
-            {
-              type: "dashboard",
-              id: "0c8aea00-174a-11e9-8fa5-c79677d29cc9",
-              attributes: {
-                title: "Nyx API",
-                hits: 0,
-                description: "",
-                panelsJSON:
-                  '[{"embeddableConfig":{},"gridData":{"x":0,"y":12,"w":18,"h":27,"i":"1"},"id":"235bc8e0-1749-11e9-8fa5-c79677d29cc9","panelIndex":"1","type":"visualization","version":"6.5.4"},{"embeddableConfig":{},"gridData":{"x":0,"y":0,"w":48,"h":12,"i":"2"},"id":"6aaced00-1749-11e9-8fa5-c79677d29cc9","panelIndex":"2","type":"visualization","version":"6.5.4"},{"embeddableConfig":{},"gridData":{"x":18,"y":25,"w":15,"h":14,"i":"3"},"id":"cdba9780-1749-11e9-8fa5-c79677d29cc9","panelIndex":"3","type":"visualization","version":"6.5.4"},{"embeddableConfig":{},"gridData":{"x":33,"y":25,"w":15,"h":14,"i":"4"},"id":"ddc92830-1749-11e9-8fa5-c79677d29cc9","panelIndex":"4","type":"visualization","version":"6.5.4"},{"gridData":{"x":18,"y":12,"w":30,"h":13,"i":"5"},"version":"6.5.4","panelIndex":"5","type":"visualization","id":"a863c3c0-174a-11e9-8fa5-c79677d29cc9","embeddableConfig":{}}]',
-                optionsJSON:
-                  '{"darkTheme":false,"hidePanelTitles":true,"useMargins":true}',
-                version: 1,
-                timeRestore: false,
-                kibanaSavedObjectMeta: {
-                  searchSourceJSON:
-                    '{"query":{"language":"lucene","query":""},"filter":[]}'
-                }
-              },
-              updated_at: "2019-01-13T15:54:24.567Z",
-              version: 4
-            },
-            {
-              type: "dashboard",
-              id: "c0beaa00-1e18-11e9-8fa5-c79677d29cc9",
-              attributes: {
-                title: "Nyx User Statistics",
-                hits: 0,
-                description: "",
-                panelsJSON:
-                  '[{"embeddableConfig":{},"gridData":{"x":0,"y":0,"w":48,"h":13,"i":"1"},"id":"30d22e80-1e18-11e9-8fa5-c79677d29cc9","panelIndex":"1","type":"visualization","version":"6.5.4","title":"Statistics"},{"embeddableConfig":{},"gridData":{"x":0,"y":13,"w":24,"h":15,"i":"2"},"id":"5cb9ce40-1e18-11e9-8fa5-c79677d29cc9","panelIndex":"2","title":"Users","type":"visualization","version":"6.5.4"},{"embeddableConfig":{},"gridData":{"x":24,"y":13,"w":24,"h":15,"i":"3"},"id":"cdba9780-1749-11e9-8fa5-c79677d29cc9","panelIndex":"3","title":"Platforms","type":"visualization","version":"6.5.4"}]',
-                optionsJSON:
-                  '{"darkTheme":false,"hidePanelTitles":false,"useMargins":true}',
-                version: 1,
-                timeRestore: false,
-                kibanaSavedObjectMeta: {
-                  searchSourceJSON:
-                    '{"query":{"language":"lucene","query":""},"filter":[]}'
-                }
-              },
-              updated_at: "2019-01-22T07:38:47.328Z",
-              version: 1
-            },
-            {
-              type: "dashboard",
-              id: "7bbb3390-2243-11e9-9a68-772c982ed0ab",
-              attributes: {
-                title: "Docker",
-                hits: 0,
-                description: "",
-                panelsJSON:
-                  '[{"gridData":{"x":0,"y":15,"w":48,"h":22,"i":"1"},"version":"6.5.4","panelIndex":"1","type":"visualization","id":"9b91cb80-2242-11e9-9a68-772c982ed0ab","embeddableConfig":{}},{"gridData":{"x":0,"y":0,"w":48,"h":15,"i":"2"},"version":"6.5.4","panelIndex":"2","type":"visualization","id":"195fc210-2243-11e9-9a68-772c982ed0ab","embeddableConfig":{}}]',
-                optionsJSON:
-                  '{"darkTheme":false,"useMargins":true,"hidePanelTitles":false}',
-                version: 1,
-                timeRestore: false,
-                kibanaSavedObjectMeta: {
-                  searchSourceJSON:
-                    '{"query":{"query":"","language":"lucene"},"filter":[]}'
-                }
-              },
-              updated_at: "2019-01-27T14:54:44.552Z",
-              version: 1
-            }
-          ]
-        },
-        "nyx"
-      );
-
-      var url = this.$store.getters.kibanaurl + "api/spaces/space";
+    url += "&_g=(" + strRefresh + ",time:(" + timek + ")";
+    
+    if (this.currentConfig.config.hiddenQuery != undefined) {
+      url += `,query:(language:lucene,query:'${this.currentConfig.config.hiddenQuery}')`;
+    }else{
+      url += ",query:(language:lucene,query:'*')";
+    }
+    
+    url += ")";  
+    console.log('url: ', url);
+    return url;
+  },
+  
+  loadKibanaDashboards: function() {
+    this.listLoading = true;
+    this.dashboards = [];
+    var url = this.$store.getters.apiurl.replace(/\/$/,"") + 
+        this.$store.getters.kibanaurl +
+        "api/spaces/space"
 
       axios
-        .get(url)
+        .get(url, {
+          params:{
+            token : this.$store.getters.creds.token
+          }})
         .then(response => {
-          console.log("Space success...");
           this.loadKibanaDashboards2(response.data);
         })
         .catch(error => {
@@ -560,27 +337,28 @@ export default {
           }, 1500)
         });
     },
+
     loadKibanaDashboards2: async function(spaces) {
-      console.log(spaces);
       this.dashboards = [];
       for (var i in spaces) {
         var space = spaces[i];
 
-        var spaceurl = "";
-
-        if (space.id != "default") spaceurl = "s/" + space.id + "/";
-        var url =
+        var spaceurl = "s/" + space.id + "/";
+        var url = this.$store.getters.apiurl.replace(/\/$/,"") + 
           this.$store.getters.kibanaurl +
           spaceurl +
           "api/saved_objects/_find?type=dashboard&per_page=1000&page=1";
 
-        const response = await axios.get(url, {});
-
+        const response = await axios.get(url, {
+          params:{
+            token : this.$store.getters.creds.token
+          }
+        });
         this.addDashboards(response.data, space.id);
       }
-
       this.listLoading = false;
     },
+
     addDashboards: function(newdashs, space) {
       for (var i in newdashs.saved_objects) {
         var dash = newdashs.saved_objects[i];
